0983ed0023cd919fd3a6de929f811dfc59c76492
krystle Mensah <web-development@krystlemensah.co.uk>
2021-02-18 20:43:41 +0000

    update

--- index.php
+++ index.php
@@ -1,96 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <!-- css -->
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/mobile.css" />
-    <link
-      href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
-    <link rel="icon" href="favicon.ico">
-    <title>Krystle Mensah | Web Developer</title>
-  </head>
-  <body>
-    <header class="hidden-on-small-screens">
-      <div class="header-container">
-        <div class="logo-container">
-          <h1 class="logo">Krystle Mensah</h1>
-        </div>
-        <!-- logo-container -->
 
-        <nav>
-          <div class="nav-container">
-            <a href="index.php">home</a>
-            <a href="portfolio.php">portfolio</a>
-            <!-- <a href="about.php">about</a> -->
-            <a href="contact.php">contact</a>
-          </div>
-          <!-- nav-container -->
-        </nav>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <!-- css -->
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/mobile.css" />
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link rel="icon" href="favicon.ico">
+  <title>Krystle Mensah | Web Developer</title>
+</head>
+
+<body>
+  <header class="hidden-on-small-screens">
+    <div class="header-container">
+      <div class="logo-container">
+        <h1 class="logo">Krystle Mensah</h1>
       </div>
-      <!-- header-container -->
-    </header>
-
-    <!-- MOBILE -->
-    <div class="mobile-container hidden-on-large-screens">
-      <!-- Top Navigation Menu -->
-      <div class="topnav">
-        
-        <a class="mobile_brand_name" href="index.php">
-          <h1 class="logo">Krystle Mensah</h1>
-        </a><!-- moblie_brand_name -->
-        
-        <span class="open_slide">
-          <a class="hamburger_icon" href="#" onClick="openSlideMenu()">
-            <i class="fa fa-bars"></i>
-          </a><!-- onclick-->
-        </span>
-
-        <div id="side_menu" onClick="closeSlideMenu()" class="side_nav">
-          <a href="#" class="btn_close">&times;</a>  
+      <!-- logo-container -->
+      <nav>
+        <div class="nav-container">
           <a href="index.php">home</a>
           <a href="portfolio.php">portfolio</a>
           <!-- <a href="about.php">about</a> -->
           <a href="contact.php">contact</a>
         </div>
-      
-      </div><!-- topnav -->
+        <!-- nav-container -->
+      </nav>
     </div>
-    <!-- MOBILE-CONTAINER -->
+    <!-- header-container -->
+  </header>
+  <!-- MOBILE -->
+  <div class="mobile-container hidden-on-large-screens">
+    <!-- Top Navigation Menu -->
+    <div class="topnav">
 
-    <section id="showcase">
-      <div class="showcase-container">
-        <!-- DATA is a html5 attribute that allows custom stuff. data-words is an array of javascript objects -->
-        <h1 class="the-showcase-title the-page-title ">
-          Hi, I'm a
-          <span
-            class="txt-type"
-            data-wait="3000"
-            data-words='["Developer", "Designer", "Musician"]'
-          ></span>
-        </h1>
+      <a class="mobile_brand_name" href="index.php">
+        <h1 class="logo">Krystle Mensah</h1>
+      </a><!-- moblie_brand_name -->
 
-        <h2>
-          welcome to my website
-        </h2>
+      <span class="open_slide">
+        <a class="hamburger_icon" href="#" onClick="openSlideMenu()">
+          <i class="fa fa-bars"></i>
+        </a><!-- onclick-->
+      </span>
+      <div id="side_menu" onClick="closeSlideMenu()" class="side_nav">
+        <a href="#" class="btn_close">&times;</a>
+        <a href="index.php">home</a>
+        <a href="portfolio.php">portfolio</a>
+        <!-- <a href="about.php">about</a> -->
+        <a href="contact.php">contact</a>
       </div>
-      <!-- showcase-container -->
-    </section>
-    <!-- showcase -->
 
-    <footer>
-      <p>&copy;2020 Krystle Mensah | All rights reversed</p>
-    </footer>
+    </div><!-- topnav -->
+  </div>
+  <!-- MOBILE-CONTAINER -->
+  <section id="showcase">
+    <div class="showcase-container">
+      <!-- DATA is a html5 attribute that allows custom stuff. data-words is an array of javascript objects -->
+      <h1 class="the-showcase-title the-page-title ">
+        Hi, I'm a
+        <span class="txt-type" data-wait="3000" data-words='["Developer", "Designer", "Musician"]'></span>
+      </h1>
+      <h2>
+        welcome to my website
+      </h2>
+    </div>
+    <!-- showcase-container -->
+  </section>
+  <!-- showcase -->
+  <footer>
+    <p>&copy;2020 Krystle Mensah | All rights reversed</p>
+  </footer>
+  <script src="js/mobile.js"></script>
+  <script src="js/main.js"></script>
+</body>
 
-    <script src="js/mobile.js"></script>
-    <script src="js/main.js"></script>
-  </body>
 </html>
