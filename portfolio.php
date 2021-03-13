0caf158886299b7620b38f14f9b9d58141b8298e
krystle Mensah <web-development@krystlemensah.co.uk>
2021-03-13 13:45:11 +0000

    about page removed

--- portfolio.php
+++ portfolio.php
@@ -1,136 +1,132 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <!-- css -->
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/mobile.css" />
-    <link
-      href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
-    <link rel="icon" href="favicon.ico">
-    <title>Krystle Mensah | Web Developer</title>
-  </head>
-  <body>
-    <header class="hidden-on-small-screens">
-      <div class="header-container">
-        <div class="logo-container">
-          <h1 class="logo">Krystle Mensah</h1>
-        </div>
-        <!-- logo-container -->
 
-        <nav>
-          <div class="nav-container">
-            <a href="index.php">home</a>
-            <a href="portfolio.php">portfolio</a>
-            <a href="about.php">about</a>
-            <a href="contact.php">contact</a>
-          </div>
-          <!-- nav-container -->
-        </nav>
-      </div><!-- header-container -->
-    </header>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <!-- css -->
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/mobile.css" />
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link rel="icon" href="favicon.ico">
+  <title>Krystle Mensah | Web Developer</title>
+</head>
 
-    <!-- MOBILE -->
-    <div class="mobile-container hidden-on-large-screens">
-      <!-- Top Navigation Menu -->
-      <div class="topnav">
-        
-        <a class="mobile_brand_name" href="index.php">
-          <h1 class="logo">Krystle Mensah</h1>
-        </a><!-- moblie_brand_name -->
-        
-    
-        <span class="open_slide">
-          <a class="hamburger_icon" href="#" onClick="openSlideMenu()">
-            <i class="fa fa-bars"></i>
-          </a><!-- onclick-->
-        </span>
+<body>
+  <header class="hidden-on-small-screens">
+    <div class="header-container">
+      <div class="logo-container">
+        <h1 class="logo">Krystle Mensah</h1>
+      </div>
+      <!-- logo-container -->
 
-        <div id="side_menu" onClick="closeSlideMenu()" class="side_nav">
-          <a href="#" class="btn_close">&times;</a>  
+      <nav>
+        <div class="nav-container">
           <a href="index.php">home</a>
           <a href="portfolio.php">portfolio</a>
-          <!-- <a href="about.php">about</a> -->
           <a href="contact.php">contact</a>
         </div>
-          
+        <!-- nav-container -->
+      </nav>
+    </div><!-- header-container -->
+  </header>
 
-      </div><!-- topnav -->
-    </div>
-    <!-- MOBILE-CONTAINER -->
-    
-    <section class="main">
-      
-      <!-- div class container -->
-      <div class="portfolio-container">
-        <!-- <div class="grid-2"> -->
-          
-          <!-- div class center -->
-          <div class="page-title">
-            <div class="page-title-container">
-              <h1 class="the-portfolio-title the-page-title">portfolio</h1>
-            </div><!-- page-title-container -->
-          </div>
+  <!-- MOBILE -->
+  <div class="mobile-container hidden-on-large-screens">
+    <!-- Top Navigation Menu -->
+    <div class="topnav">
 
-          <div class="work">
-            <div class="work-container">
+      <a class="mobile_brand_name" href="index.php">
+        <h1 class="logo">Krystle Mensah</h1>
+      </a><!-- moblie_brand_name -->
 
-              <!--work-->
-              <a class="work_item" href="https://ccsnh.college-tour.com/" target="_blank">
-                <div class="work_item_container">
 
-                  <img class="the-work-image" src="img/work-1.jpg" alt="">
+      <span class="open_slide">
+        <a class="hamburger_icon" href="#" onClick="openSlideMenu()">
+          <i class="fa fa-bars"></i>
+        </a><!-- onclick-->
+      </span>
 
-                </div><!-- work_item_container -->
-              </a><!-- work_item -->
-              <!-- end -->
-              
-              <!--work-->
-              <a class="work_item" href="https://newman.university-tour.com/" target="_blank">
-                <div class="work_item_container">
+      <div id="side_menu" onClick="closeSlideMenu()" class="side_nav">
+        <a href="#" class="btn_close">&times;</a>
+        <a href="index.php">home</a>
+        <a href="portfolio.php">portfolio</a>
+        <!-- <a href="about.php">about</a> -->
+        <a href="contact.php">contact</a>
+      </div>
 
-                  <img class="the-work-image" src="img/newman.jpg" alt="">
 
-                </div><!-- work_item_container -->
-              </a><!-- work_item -->
-              <!-- END WORK -->
-              <!--WORK-->
-              <a class="work_item" href="https://law-wustl.university-tour.com/" target="_blank">
-                <div class="work_item_container">
+    </div><!-- topnav -->
+  </div>
+  <!-- MOBILE-CONTAINER -->
 
-                  <img class="the-work-image" src="img/washington.jpg" alt="">
+  <section class="main">
 
-                </div><!-- work_item_container -->
-              </a><!-- work_item -->
-              <!-- END WORK -->
-              <!--WORK-->
-              <a class="work_item" href="https://djgear.herokuapp.com/" target="_blank">
-                <div class="work_item_container">
+    <!-- div class container -->
+    <div class="portfolio-container">
+      <!-- <div class="grid-2"> -->
 
-                  <img class="the-work-image" src="img/djgear.jpg" alt="">
+      <!-- div class center -->
+      <div class="page-title">
+        <div class="page-title-container">
+          <h1 class="the-portfolio-title the-page-title">portfolio</h1>
+        </div><!-- page-title-container -->
+      </div>
 
-                </div><!-- work_item_container -->
-              </a><!-- work_item -->
-              <!-- END WORK -->
-          
-            </div><!-- work-container -->
-          </div><!-- work -->
-      </div><!-- portfolio-container -->
-    </section>
+      <div class="work">
+        <div class="work-container">
 
-    <footer>
-      <p>&copy;2020 Krystle Mensah | All rights reversed</p>
-    </footer>
+          <!--work-->
+          <a class="work_item" href="https://ccsnh.college-tour.com/" target="_blank">
+            <div class="work_item_container">
 
-    <script src="js/mobile.js"></script>
-    <script src="js/main.js"></script>
-  </body>
+              <img class="the-work-image" src="img/work-1.jpg" alt="">
+
+            </div><!-- work_item_container -->
+          </a><!-- work_item -->
+          <!-- end -->
+
+          <!--work-->
+          <a class="work_item" href="https://newman.university-tour.com/" target="_blank">
+            <div class="work_item_container">
+
+              <img class="the-work-image" src="img/newman.jpg" alt="">
+
+            </div><!-- work_item_container -->
+          </a><!-- work_item -->
+          <!-- END WORK -->
+          <!--WORK-->
+          <a class="work_item" href="https://law-wustl.university-tour.com/" target="_blank">
+            <div class="work_item_container">
+
+              <img class="the-work-image" src="img/washington.jpg" alt="">
+
+            </div><!-- work_item_container -->
+          </a><!-- work_item -->
+          <!-- END WORK -->
+          <!--WORK-->
+          <a class="work_item" href="https://djgear.herokuapp.com/" target="_blank">
+            <div class="work_item_container">
+
+              <img class="the-work-image" src="img/djgear.jpg" alt="">
+
+            </div><!-- work_item_container -->
+          </a><!-- work_item -->
+          <!-- END WORK -->
+
+        </div><!-- work-container -->
+      </div><!-- work -->
+    </div><!-- portfolio-container -->
+  </section>
+
+  <footer>
+    <p>&copy;2020 Krystle Mensah | All rights reversed</p>
+  </footer>
+
+  <script src="js/mobile.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
 </html>
