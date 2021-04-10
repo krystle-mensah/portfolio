5a72e8f97df38fee96e74f523d301a6856481171
krystle Mensah <web-development@krystlemensah.co.uk>
2021-04-10 17:43:13 +0100

    postgres added

---
+++ admin/db.php
@@ -0,0 +1,17 @@
+<?php
+// Local Database Credentials
+define('DB_HOST', 'ec2-52-45-73-150.compute-1.amazonaws.com');
+define('DB_USER', 'htcsnjphogatig');
+define('DB_PASS', '984efae787add6b83ffc078408f64aeb692cf8a14b2250c9b46f926a205a8fa1');
+define('DB_NAME', 'd59lv0gcpo9vjt');
+define('THE_PORT', '5432');
+
+// Create connection
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, THE_PORT);
+
+if (mysqli_connect_errno()) {
+  printf("Connect failed: %s\n", mysqli_connect_error());
+  exit();
+} else {
+  echo "Connected successfully";
+}
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php include('admin/db.php'); ?>
 
 <head>
   <meta charset="UTF-8" />
